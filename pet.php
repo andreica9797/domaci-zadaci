e6aa2b8949b12f085ebc55f183d0ac2200f31e33
andreica9797 <57364335+andreica9797@users.noreply.github.com>
2019-11-10 22:57:21 +0100

    Add files via upload

---
+++ pet.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+
+<html>
+<head>
+<title>Login Page </title>
+
+<h1>Harry Potter Quiz</h1>
+<div> 
+<h2>Which pet would you bring with you in Hogwarts?</h2>
+
+
+<br>
+<input type="radio" name="gender" value="male">Owl
+</br>
+<br>
+<input type="radio" name="gender" value="female"> Cat
+</br>
+<br>
+<input type="radio" name="gender" value="other"> Spider
+</br>
+<br>
+<input type="radio" name="gender" value="female"> Toad
+</br>
+
+
+ 
+
+
+<br>
+<A HREF=result.php>Continue</A>
+</br>
+
